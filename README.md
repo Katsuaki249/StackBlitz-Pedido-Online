8192631cd1673aaabc435fcbf460cf8e34b08609
Katsuaki249 <vinicius.katsuaki13@hotmail.com>
2023-05-08 17:30:26 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# StackBlitz-Pedido-Online
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-deupvj)
---
+++ pedidoonline/src/components/Sidebar/index.tsx
@@ -0,0 +1,20 @@
+import { Container } from './styles';
+
+import {} from '../../assets/burger.svg';
+
+export function Sidebar() {
+  return (
+    <Container>
+      <button>x</button>
+      <nav>
+        <ul>
+          <li>
+            <a href="#">
+              <span></span>
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </Container>
+  );
+}
